66af5c829094dabdd1c0bd2e548167ed199ebdd6
Polyane Ribeiro <poly.pequena@gmail.com>
2023-03-01 15:18:17 -0300

    alterações modal fechar pedido

--- components/ModalPedido/index.tsx
+++ components/ModalPedido/index.tsx
@@ -22,7 +22,6 @@ export const ModalPedido = ({ open, success, action }: ModalPedidoProps) => {
                                 <BsCheckCircle color="#ff9999" size={40} />
                                 <S.Destaque> Seu pedido foi enviado com SUCESSO para o vendedor.</S.Destaque>
                                 <p><b> Em breve ele entrará em contato.</b></p>
-                                <p> Continue navegando pelo nosso site e veja as TENDÊNCIAS para esse ano.</p>
                                 <Button text='ok' action={() => Submit('/')} width='78px' />
                             </>
                         ) : (
